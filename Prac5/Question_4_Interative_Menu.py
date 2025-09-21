9515dd259066e651224be2e2ab424f3577e9e8d2
LukeRiedel <lukeriedel5@gmail.com>
2025-09-21 23:40:07 +0800

    Finished interactive menu thank you very much

--- Prac5/Question_4_Interative_Menu.py
+++ Prac5/Question_4_Interative_Menu.py
@@ -12,7 +12,7 @@ def main():
     option = None
 
     while option != "E":
-        print("\n--------INTERACTIVE LINKED LIST--------\n")
+        print("\n\n--------INTERACTIVE LINKED LIST--------\n")
         print("\nPick an option \n"
         "\n\t(A) Add \n\t(R) Remove  \n\t(D) Display  \n\t(E) Exit\n")
 
@@ -22,32 +22,49 @@ def main():
         if option == "E" : 
             break 
         elif option == "A" :
-            ans = input("Pick an integer to insert\t")
-            binarySearchTree.insert(int(ans), "Value")
-        elif option == "R" : 
-            binarySearchTree.inOrderTraveral()
-            ans = input("Please select a number from the list above. (Type 'end' to exit)\n")
-            if ans == "end":
+            try: 
+                ans = input("Pick an integer to insert:\t")
+                binarySearchTree.insert(int(ans), "Value")
+            except ValueError:
+                print("\nValueError: Input was not valid, please try again.")
                 pass
-            else: 
-                binarySearchTree.delete(int(ans))
+        elif option == "R" :
+            try: 
+                if binarySearchTree.count() == 0:
+                    print("RemoveError: Cannot remove, BST is empty. Please Add (A) a value first.")
+                    pass
+                else:
+                    binarySearchTree.inOrderTraveral()
+                    ans = input("\nPlease select a number from the list above. (Type 'end' to exit)\t")
+                    if ans == "end":
+                        pass
+                    else: 
+                        binarySearchTree.delete(int(ans))
+            except ValueError:
+                print("\nValueError: Input was not an integer, please try again.")
         elif option == "D" : 
-            print("Please pick a number from the following options:")
-            print("\n\t1.\tIn-Order Traversal")
-            print("\n\t2.\tPre-Order Traversal")
-            print("\n\t3.\tPost-Order Traversal")
-            ans = int(input("\n\t"))
-            if ans == 1:
-                print("\nIn-Order Traversal sequence:\t", end = '')
-                binarySearchTree.inOrderTraveral()
-            elif ans == 2:
-                print("\nPre-Order Traversal sequence:\t", end = '')
-                binarySearchTree.preOrderTraveral()
-            elif ans == 3:
-                print("\nPost-Order Traversal sequence:\t", end = '')
-                binarySearchTree.postOrderTraveral()
-            else:
-                print("InputError: Please pick one of the listed options. ")
+            try:
+                if binarySearchTree.count() == 0:
+                    print("\nDisplayError: BST is empty - Nothing to Display.")
+                else:
+                    print("Please pick a number from the following options:")
+                    print("\n\t1.\tIn-Order Traversal")
+                    print("\n\t2.\tPre-Order Traversal")
+                    print("\n\t3.\tPost-Order Traversal")
+                    ans = int(input("\n\t"))
+                    if ans == 1:
+                        print("\nIn-Order Traversal sequence:\t", end = '')
+                        binarySearchTree.inOrderTraveral()
+                    elif ans == 2:
+                        print("\nPre-Order Traversal sequence:\t", end = '')
+                        binarySearchTree.preOrderTraveral()
+                    elif ans == 3:
+                        print("\nPost-Order Traversal sequence:\t", end = '')
+                        binarySearchTree.postOrderTraveral()
+                    else:
+                        print("\nInputError: Please pick one of the provided options.")
+            except ValueError:
+                print("\nValueError: Input was not valid. ")
         else: 
             print("InputError: Please enter one of the provided functions")
     return
